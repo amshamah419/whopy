d353d160110baf4d84cbce6c3994b9c333201e57
Andrew Shamah <andrew@demisto.com>
2019-04-22 13:55:12 +0300

    Add descriptions to readme

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='whopy',
       version='0.1',
-      description='Module for retrieving and parsing the WHOIS data for a domain. Supports most domains. No dependencies.',
+      description='Module for retrieving and parsing the WHOIS data for a domain. Supports most domains.',
       author='Andrew Shamah',
       author_email='amshamah@gmail.com',
-      url='http://cryto.net/whopy',
+      url='https://github.com/amshamah419/whopy',
       packages=['whopy'],
       package_dir={"whopy":"whopy"},
       package_data={"whopy": ["*.dat", "*.json"]},
